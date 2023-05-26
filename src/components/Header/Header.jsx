0bebca6d80c929629facccc5655efead0ec074fe
Marcosotola <111930679+Marcosotola@users.noreply.github.com>
2023-05-26 10:28:10 -0300

    Bancor obra

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <>
       <div className='header'>
-        <h4>OBRA ROGGIO</h4>
+        <h4>OBRA: BANCOR</h4>
         <div className='logoContainer'>
           <img src={Logo} className='logo' alt="logo" />
         </div>
