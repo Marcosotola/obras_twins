a0f8070abfbe644b7cd0b72ab5ea72569c04d3fa
Marcosotola <marcosotola@gmail.com>
2023-10-31 15:54:40 -0300

    botones de enlaces en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,35 @@ import bgLogo from "./bgLogo.png";
 const Home = () => {
   return (
     <>
-      <h1>It is Home</h1>
       <div class="container">
+
         <div
-          className="bg-image text-center"
+          className="bg-image text-center pt-3"
           style={{
             backgroundImage: `url(${bgLogo})`,
             backgroundAttachment: "fixed",
-            backgroundSize: "50%",
+            backgroundSize: "60%",
             backgroundPosition: "center",
-            backgroundRepeat: "no-repeat"  
+            backgroundRepeat: "no-repeat",
           }}
         >
-
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad obcaecati asperiores praesentium temporibus qui similique magnam, pariatur voluptates totam dolorem magni reiciendis nemo ullam blanditiis omnis veniam quasi eum! Quidem!
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum excepturi asperiores accusamus delectus ut et ipsam ab tenetur consectetur atque illo est dignissimos ex, recusandae iste pariatur, laboriosam commodi. Delectus!
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos repellat dolorem laudantium beatae obcaecati, voluptatum dolores eum aliquid inventore! Neque quos repellat voluptate ex modi consequatur expedita sint tenetur tempora!
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam tenetur porro, repudiandae deleniti, iste commodi obcaecati iusto, aperiam soluta consequuntur nobis itaque sint officia odio asperiores exercitationem ab veniam! Repellat.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, delectus. Deserunt adipisci atque blanditiis, beatae porro modi facilis veniam eligendi amet quo earum voluptatem? Commodi nihil magni laborum quibusdam illo!
-
-
-
-
-
+             <h1 className="text-center bg-secondary rounded mb-5">P O C I T O</h1>
+          <div class="d-grid gap-4 mt-3">
+            <button class="btn btn-lg btn-primary" type="button">
+             <a className="text-decoration-none" href="/Tareas">Tareas Diarias</a> 
+            </button>
+            <button class="btn btn-lg btn-primary" type="button">
+            <a className="text-decoration-none" href="/Materiales">Movimiento Materiales</a> 
+            </button>
+            <button class="btn btn-lg btn-primary" type="button">
+              Avance Obra
+            </button>
+            <button class="btn btn-lg btn-primary" type="button">
+              Planos PDF
+            </button>
+          </div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, cupiditate. Quisquam rem porro minima quos laboriosam in cupiditate itaque voluptatum possimus enim esse exercitationem veniam illo maiores, at, odio ullam!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat labore iure numquam sapiente voluptatem ipsa ducimus repudiandae odio est saepe voluptas, perferendis voluptates consectetur similique velit doloremque dolorem ipsum!
         </div>
       </div>
     </>
--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -5,9 +5,8 @@ import Menu from '../../components/Menu';
 export const Header = () => {
   return (
     <>
-      <div className='d-flex justify-content-around align-items-center'>
+      <div className='d-flex justify-content-between align-items-center mt-3  p-1'>
         <Menu />
-        <h3 className='text-secondary'>POCITO</h3>
         <div className='bg-secondary p-1 rounded'>
           <img src={Logo} className="logo" alt="logo" width='100vh'/>
         </div>
