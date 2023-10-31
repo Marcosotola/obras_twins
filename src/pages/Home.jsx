3c321ac4e4c96f7e3046f15d20581931e0ff2181
Marcosotola <marcosotola@gmail.com>
2023-10-31 14:53:34 -0300

    bg image en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import bgLogo from "./bgLogo.png";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <h1>It is Home</h1>
-    </div>
-  )
-}
+      <div class="container">
+        <div
+          className="bg-image text-center"
+          style={{
+            backgroundImage: `url(${bgLogo})`,
+            backgroundAttachment: "fixed",
+            backgroundSize: "50%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat"  
+          }}
+        >
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad obcaecati asperiores praesentium temporibus qui similique magnam, pariatur voluptates totam dolorem magni reiciendis nemo ullam blanditiis omnis veniam quasi eum! Quidem!
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum excepturi asperiores accusamus delectus ut et ipsam ab tenetur consectetur atque illo est dignissimos ex, recusandae iste pariatur, laboriosam commodi. Delectus!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos repellat dolorem laudantium beatae obcaecati, voluptatum dolores eum aliquid inventore! Neque quos repellat voluptate ex modi consequatur expedita sint tenetur tempora!
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam tenetur porro, repudiandae deleniti, iste commodi obcaecati iusto, aperiam soluta consequuntur nobis itaque sint officia odio asperiores exercitationem ab veniam! Repellat.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, delectus. Deserunt adipisci atque blanditiis, beatae porro modi facilis veniam eligendi amet quo earum voluptatem? Commodi nihil magni laborum quibusdam illo!
+
+
+
+
+
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
