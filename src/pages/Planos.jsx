eddd8113d27f2a5ba63a0fa4a645212ee1088bb1
Marcosotoladev <marcosotoladev@gmail.com>
2024-01-13 19:48:28 -0300

    change page url plans

--- src/pages/Planos.jsx
+++ src/pages/Planos.jsx
@@ -6,12 +6,12 @@ const PLanos = () => {
       <div className="container">
         <div class="d-grid gap-4 mt-5 mb-5">
           <button class="btn btn-lg btn-primary" type="button">
-          <a className="text-decoration-none" href="https://drive.google.com/drive/folders/1OPWFHpJM6bbNRBUb6y1cpNludaBD1CQI?usp=drive_link">
+          <a className="text-decoration-none" href="https://drive.google.com/drive/u/0/folders/1AHTpqbLVdKA4PgVAsaEKep9dwLV1E3bj">
             <h3>D W G</h3>
             </a>
           </button>
           <button class="btn btn-lg btn-primary" type="button">
-          <a className="text-decoration-none" href="https://drive.google.com/drive/folders/1ADTj6j6CDmcUvOVSYYJmY7TW1hFzyhZt?usp=drive_link">
+          <a className="text-decoration-none" href="https://drive.google.com/drive/u/0/folders/1ANr3ruMgZgNc8mGiLL7TPZE_8vLSG7dU">
             <h3>P D F</h3>
             </a>
           </button>
